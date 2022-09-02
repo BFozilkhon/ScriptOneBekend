6e9b5ea7cd379e69f85ac19b915700f2f8c017d3
Fozilkhon <buzrukxojayevfozilxon2007@gmail.com>
2022-09-02 05:05:04 -0700

    husanboy

--- src/index.js
+++ src/index.js
@@ -14,3 +14,17 @@ root.render(
 );
 
 
+// Boshlangich configlar: 
+// 1.Git bash ornatib olib
+// 2.git config --global user.name "BFozilkhon"
+// 3.git config --global user.email "muhammadrasul12@gmail.com"
+// 4.Githubni logindan otib olish
+
+// Github ulash
+// 1.Git repozitory
+// 2.git init 
+// 3.git remote add origin https://github.com/BFozilkhon/ScriptOneBekend.git
+// 4.git remote -v
+// 5.git add .
+// 6.git commit -m 'nimadur text'
+// 7.git push origin master
